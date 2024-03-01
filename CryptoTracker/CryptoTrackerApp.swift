e89f8a2b3ff2c4ece2c40a0c3dce3269939bd019
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-02 03:28:27 +0600

    first commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

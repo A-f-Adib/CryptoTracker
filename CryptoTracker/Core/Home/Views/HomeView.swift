e89870064d2a77ec807d7dfa9172569624c61c3b
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-03 20:12:35 +0600

    HomeView added

---
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 3/3/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Header")
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}

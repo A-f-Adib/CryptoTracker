0c66c9b637dbcf27c57c43589b192e44ab744d9d
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-04 02:33:36 +0600

    coinmodel added

--- CryptoTracker/Models/CoinModel.swift
+++ CryptoTracker/Models/CoinModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-class CoinModel {
+struct CoinModel {
+        let id, symbol, name: String?
+        let image: String?
+        let currentPrice : Double
+        let marketCap, marketCapRank, fullyDilutedValuation: Double?
+        let totalVolume, high24H, low24H: Double?
+        let priceChange24H, priceChangePercentage24H: Double?
+        let marketCapChange24H: Double?
+        let marketCapChangePercentage24H: Double?
+        let circulatingSupply, totalSupply, maxSupply, ath: Double?
+        let athChangePercentage: Double?
+        let athDate: String?
+        let atl, atlChangePercentage: Double?
+        let atlDate: String?
+        let lastUpdated: String?
+        let priceChangePercentage24HInCurrency: Double?
     
+
 }

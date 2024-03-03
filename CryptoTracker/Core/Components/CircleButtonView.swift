7d1b475ae057873aa587d354c5038437e7a5cdcb
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-03 20:48:43 +0600

    circlebutton added

--- CryptoTracker/Core/Components/CircleButtonView.swift
+++ CryptoTracker/Core/Components/CircleButtonView.swift
@@ -10,15 +10,29 @@ import SwiftUI
 struct CircleButtonView: View {
     var body: some View {
        Image(systemName: "heart.fill")
-            .font(.headline)
+            .font(.title3)
             .foregroundColor(Color.theme.accent)
+            .frame(width : 50, height: 50)
+            .background(
+             Circle()
+                .foregroundColor(Color.theme.background)
+            )
+            .shadow(color: Color.theme.accent.opacity(0.25), radius: 10, x: 0, y: 0)
+            .padding()
     }
 }
 
 struct CircleButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleButtonView()
-            .padding()
-            .previewLayout(.sizeThatFits)
+        Group {
+            CircleButtonView()
+                .padding()
+                .previewLayout(.sizeThatFits)
+            
+            CircleButtonView()
+                .padding()
+                .previewLayout(.sizeThatFits)
+                .colorScheme(.dark)
+        }
     }
 }

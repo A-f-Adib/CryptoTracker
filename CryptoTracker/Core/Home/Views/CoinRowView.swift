ae1c0d4c45964385f2bda15275bf7554d852ada4
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-04 03:12:11 +0600

    update previewprovider

--- CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CoinRowView: View {
+    
+    let coin : CoinModel
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +18,6 @@ struct CoinRowView: View {
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView()
+        CoinRowView(coin: dev.coin)
     }
 }

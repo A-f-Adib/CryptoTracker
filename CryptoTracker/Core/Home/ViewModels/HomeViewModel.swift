3ee011112bfc2b77d18adf7997244ebc93f0305a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-08 23:44:59 +0600

    hmViewModel updated

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,10 +12,13 @@ class HomeViewModel : ObservableObject {
     @Published var allCoins : [ CoinModel] = []
     @Published var portfolioCoins : [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers () {
+        dataService.$allCoins
     }
 }

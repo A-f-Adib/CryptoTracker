76bba5ece9db952b0dbf614601c07dd15018bdaf
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-31 03:37:39 +0600

    error handeler added

--- CryptoTracker/Utilities/NetworkingManager.swift
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -10,6 +10,18 @@ import Combine
 
 class NetworkingManager {
     
+    enum NetworkingError: LocalizedError {
+        case badUrlResponse
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badUrlResponse : return "Bad url response"
+            case .unknown : return "Unknown Error"
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
@@ -22,7 +34,7 @@ class NetworkingManager {
     static func handleUrlResponse(output:  URLSession.DataTaskPublisher.Output) throws -> Data {
         guard let respnse = output.response as? HTTPURLResponse,
               respnse.statusCode >= 200 && respnse.statusCode < 300 else {
-                  throw URLError(.badServerResponse)
+                  throw NetworkingError.badUrlResponse
               }
         return output.data
         

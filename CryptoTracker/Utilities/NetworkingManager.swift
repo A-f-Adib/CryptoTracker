425252fe7ca74c662a531debb20869619084d755
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-03-31 02:13:33 +0600

    updated

--- CryptoTracker/Utilities/NetworkingManager.swift
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output)-> Data in
+                
+                guard let respnse = output.response as? HTTPURLResponse,
+                      respnse.statusCode >= 200 && respnse.statusCode < 300 else {
+                          throw URLError(.badServerResponse)
+                      }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}

6a42dcf501b4ad2973b695e063c499ec469b82c8
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-01 22:21:12 +0600

    coinviewmodel added

---
+++ CryptoTracker/Core/Components/CoinImageView.swift
@@ -0,0 +1,27 @@
+//
+//  CoinImageView.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 4/1/24.
+//
+
+import SwiftUI
+
+class coinImageViewModel : ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading : Bool = false
+    
+}
+
+struct CoinImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView()
+    }
+}

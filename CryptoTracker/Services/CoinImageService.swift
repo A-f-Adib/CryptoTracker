4dcff44a79e05ffefa2e7bcb06d748803ea8ef15
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-01 23:53:40 +0600

    updated

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -6,8 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
  
 class CoinImageService {
+    
+    @Published var image: UIImage? = nil
     
     init() {
         getCoins()
@@ -15,5 +18,16 @@ class CoinImageService {
     
     private func getCoins() {
         
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+        else {
+            return
+        }
+        
+        coinSubscribtion =  NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscribtion?.cancel()
+            })
     }
 }

4bc536175021731ee551429eec0b7fa07f8748b7
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-02 23:47:32 +0600

    updated

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -23,7 +23,7 @@ class CoinImageService {
             return
         }
         
-        coinSubscribtion =  NetworkingManager.download(url: url)
+            =  NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins

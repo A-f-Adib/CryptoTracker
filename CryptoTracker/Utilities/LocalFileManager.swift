1102546a560dfbe8b0396f36ad13c7c824570c1c
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-18 05:54:40 +0600

    localfilemanager added

---
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocalFileManger.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 4/18/24.
+//
+
+import Foundation
+
+//Save image and get image from file manager
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() { }
+    
+    
+}

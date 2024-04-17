901e3e4111f933d8e2286bf57b615fe8d40a29f8
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-18 04:31:31 +0600

    updated

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -7,27 +7,33 @@
 
 import Foundation
 import SwiftUI
+import Combine
  
 class CoinImageService {
     
     @Published var image: UIImage? = nil
     
-    init() {
-        getCoins()
+    var ImageSubscription : AnyCancellable?
+    
+    
+    init(urlString: String) {
+        getCoinImage(urlString: urlString)
     }
     
-    private func getCoins() {
+    private func getCoinImage(urlString : String) {
         
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+        guard let url = URL(string: urlString)
         else {
             return
         }
         
-            =  NetworkingManager.download(url: url)
-            .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins
-                self?.coinSubscribtion?.cancel()
+           ImageSubscription =  NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.ImageSubscription?.cancel()
             })
     }
 }

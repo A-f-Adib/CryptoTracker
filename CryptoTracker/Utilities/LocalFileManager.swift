64c62aae6ee529a5cd5ae0ef9c09bf74e49c2b1a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-19 03:54:17 +0600

    create func added

--- CryptoTracker/Utilities/LocalFileManager.swift
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -15,10 +15,10 @@ class LocalFileManager {
     static let instance = LocalFileManager()
     private init() { }
     
-    func saveImage(image: UIImage) {
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
         
         guard let data = image.pngData(),
-              let url = URL(string: "")
+              let url = getUrlForImage(folderName: folderName, imageName: imageName)
         else {
             return
         }
@@ -28,6 +28,20 @@ class LocalFileManager {
             try data.write(to: url)
         } catch let error {
             print("Error saving image \(error)")
+        }
+    }
+    
+    
+    private func createFolderIfneed(folderName : String) {
+        guard let url = getUrlForFolder(folderName: folderName) else {
+            return
+        }
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating folder \(error)")
+            }
         }
     }
     

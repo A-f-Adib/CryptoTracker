8ca3b1a0b028d62b96fd127b8c675ce7d308a1fe
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-20 02:02:48 +0600

    search added

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -23,6 +23,7 @@ struct HomeView: View {
             VStack {
                 
                 homeHeader
+                SearchbarView(searchText: $vm.searchText)
                 columnTitles
                 
                 if !showPortfolio {

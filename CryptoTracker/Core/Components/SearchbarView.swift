2b1dc77f3fba43c663c1758e4df6788544605c1a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-20 01:49:11 +0600

    updated

--- CryptoTracker/Core/Components/SearchbarView.swift
+++ CryptoTracker/Core/Components/SearchbarView.swift
@@ -15,8 +15,18 @@ struct SearchbarView: View {
       
         HStack {
             Image(systemName: "magnifyingglass")
-                .foregroundColor(.gray)
+                .foregroundColor(searchText.isEmpty ? .gray : Color.theme.accent)
+            
             TextField("Search Coin", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .overlay(
+                  Image(systemName: "xmark.circle.fill")
+                    .padding()
+                    .offset(x: 10)
+                    .foregroundColor(Color.theme.accent)
+                    .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                  , alignment: .trailing
+                )
         }
         .font(.headline)
         .padding()
@@ -25,6 +35,7 @@ struct SearchbarView: View {
                 .fill(Color.theme.background)
                 .shadow(color: Color.theme.accent.opacity(0.2), radius: 10, x: 0, y: 0)
         )
+        .padding()
     }
 }
 

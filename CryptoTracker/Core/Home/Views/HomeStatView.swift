ba230ab6b397d03e5e83960f31a3999112cb95d5
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-27 02:24:24 +0600

    updated

--- CryptoTracker/Core/Home/Views/HomeStatView.swift
+++ CryptoTracker/Core/Home/Views/HomeStatView.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 
 struct HomeStatView: View {
-    let statistics : [StatisticModel] = [
-    StatisticModel(title: "Title", value: "Value", percentageChange:1),
-    StatisticModel(title: "Title", value: "Value"),
-    StatisticModel(title: "Title", value: "Value"),
-    StatisticModel(title: "Title", value: "Value", percentageChange:-7)
-    ]
+    
+    @EnvironmentObject private var vm : HomeViewModel
+    @Binding var showPortfolio : Bool
+    
     var body: some View {
         HStack {
-            ForEach(statistics) { stat in
+            ForEach(vm.statistics) { stat in
                 StatisticView(stat: stat)
+                    .frame(width: UIScreen.main.bounds.width / 3)
             }
         }
+        .frame(width: UIScreen.main.bounds.width,
+               alignment: showPortfolio ? .trailing : .leading)
     }
 }
 
 struct HomeStatView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeStatView()
+        HomeStatView(showPortfolio: .constant(false))
+            .environmentObject(dev.homeVM)
     }
 }

ef99abc511f273a5aa4be4c56cd5b87cd004dbc4
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-04-30 02:32:17 +0600

    searchbar added

--- CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct PortfolioView: View {
+    
+    @EnvironmentObject private var vm : HomeViewModel
+    
     var body: some View {
         NavigationView{
             ScrollView{
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("hello")
+                    SearchbarView(searchText: $vm.searchText)
+                    
+                    ScrollView (.horizontal, showsIndicators: true, content: {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                Text(coin.symbol.uppercased())
+                            }
+                        }
+                    })
                 }
             }
             .navigationTitle("Edit portfolio")
@@ -29,5 +40,6 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }

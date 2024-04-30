97797104e52de36d65f1fe82f4ccb1c997a2a840
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-05-01 04:39:34 +0600

    pfds updated

--- CryptoTracker/Services/PortfolioDataService.swift
+++ CryptoTracker/Services/PortfolioDataService.swift
@@ -12,8 +12,26 @@ class PortfolioDataService {
     
     private let container : NSPersistentContainer
     private let containerName : String = "PortfolioContainer"
+    private let entityName : String = "PortfolioEntity"
+    
+    @Published var savedEntity : [ PortfolioEntity] = []
     
     init() {
         container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                print("Error loading Core Data! \(error)")
+            }
+        }
+    }
+    
+    
+    private func getPortfolio() {
+        let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
+        do {
+            savedEntity = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching Portfolio Entities. \(error)")
+        }
     }
 }

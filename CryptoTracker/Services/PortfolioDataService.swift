31ef0b3cd8407cbc8ef4dbd66c7d42de9a7402b1
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-05-01 05:20:28 +0600

    changed

--- CryptoTracker/Services/PortfolioDataService.swift
+++ CryptoTracker/Services/PortfolioDataService.swift
@@ -22,6 +22,7 @@ class PortfolioDataService {
             if let error = error {
                 print("Error loading Core Data! \(error)")
             }
+            self.getPortfolio()
         }
     }
     

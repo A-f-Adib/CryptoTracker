0e81919afd0e80615fe0a51a3dfbed4ba108ef1a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-05-12 20:16:54 +0600

    detailView added

---
+++ CryptoTracker/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 5/12/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}

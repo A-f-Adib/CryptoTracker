5cdabc25d93b69446cfb4f9494dc7fa119825664
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-05-12 21:31:27 +0600

    model updated

--- CryptoTracker/Models/CoinDetailModel.swift
+++ CryptoTracker/Models/CoinDetailModel.swift
@@ -7,20 +7,32 @@
 
 import Foundation
 
-struct CoinDetailModel {
+struct CoinDetailModel: Codable {
     let id, symbol, name: String?
     let blockTimeInMinutes : Int?
     let hashingAlgorithm: String?
-    let categories: [String]?
-    let welcomeDescription: Description?
+    let description: Description?
     let links: Links?
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, description, links
+        case blockTimeInMinutes = "block_time_in_minutes"
+        case hashingAlgorithm = "hashing_algorithm"
+        
+    }
 }
 
-struct Links {
+struct Links: Codable {
     let homepage: [String]?
     let subredditURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case homepage
+        case subredditURL = "subreddit_url"
+    }
 }
 
-struct Description {
+struct Description: Codable {
     let en: String?
 }

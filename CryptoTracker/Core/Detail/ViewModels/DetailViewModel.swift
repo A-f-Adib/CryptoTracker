89313a43cac562b09ccdd591d68bc2e2be2123ef
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-05-14 01:34:44 +0600

    func added

--- CryptoTracker/Core/Detail/ViewModels/DetailViewModel.swift
+++ CryptoTracker/Core/Detail/ViewModels/DetailViewModel.swift
@@ -6,7 +6,19 @@
 //
 
 import Foundation
+import Combine
 
 class DetailViewModel {
     
+    private let coinDetailService : CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        
+    }
 }

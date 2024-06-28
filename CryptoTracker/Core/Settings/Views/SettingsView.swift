98ddf6a008a10379b766a827341516d95265d23a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-06-28 21:52:21 +0600

    settings view added

---
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  CryptoTracker
+//
+//  Created by A.f. Adib on 6/28/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}

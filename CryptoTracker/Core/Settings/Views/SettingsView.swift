98f07b926029efa445e2f0b2481c3c5c65b046af
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-06-28 22:08:52 +0600

    updated

--- CryptoTracker/Core/Settings/Views/SettingsView.swift
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfulthinking")!
+    let coffeeURL = URL(string: "https://www.buymeacoffee.com/nicksarno")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://www.nicksarno.com")!
+    
     var body: some View {
         NavigationView{
             List{
              
-                Section(header: Text("Header"), footer: Text("Footer")) {
-                    Text("Hi")
-                    Text("Hi")
+                Section(header: Text("Swiftful Thinking"), footer: Text("Footer")) {
+                    VStack(alignment: .leading) {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
                 }
             }
             .navigationTitle("Settings")

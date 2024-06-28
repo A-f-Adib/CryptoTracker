b4907156bf6d9561dafc93a8a2fa2e8b27ad7dcf
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-06-28 22:40:01 +0600

    developer section added

--- CryptoTracker/Core/Settings/Views/SettingsView.swift
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -13,13 +13,15 @@ struct SettingsView: View {
     let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfulthinking")!
     let coffeeURL = URL(string: "https://www.buymeacoffee.com/nicksarno")!
     let coingeckoURL = URL(string: "https://www.coingecko.com")!
-    let personalURL = URL(string: "https://www.nicksarno.com")!
+    let personalURL = URL(string: "https://a-f-adib.github.io/resume/")!
     
     var body: some View {
         NavigationView{
             List{
                     swiftfulthinkingSection
                     coingeckoSection
+                    developerSection
+                    applicationSection
             }
             .font(.headline)
             .accentColor(.blue)
@@ -76,8 +78,38 @@ extension SettingsView {
                     .foregroundColor(Color.theme.accent)
             }
             .padding(.vertical)
-            Link("Subscribe on Yutube", destination: youtubeURL)
-            Link("Support coffee addiction", destination: coffeeURL)
+            Link("Visit Coingecko", destination: coingeckoURL)
         }
     }
+    
+    private var developerSection: some View {
+        
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("avatar")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was by A.F.Adib an passionate iOS developer. It uses MVVM Architecture, Combine and CoreData.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit Adib's portfolio", destination: personalURL)
+        }
+    }
+    
+    private var applicationSection: some View {
+        
+        Section(header: Text("About the app")) {
+            Link("Terms of service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+    
+    
 }
